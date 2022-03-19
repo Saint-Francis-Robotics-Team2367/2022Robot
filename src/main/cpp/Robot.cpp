a4b4393b3939932156b59724b63ab9f6849aa74c
sidhullur12 <51142971+sidhullur12@users.noreply.github.com>
2022-03-19 12:54:11 -0700

    Full Update with main

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -60,3 +60,22 @@ void Robot::AutonomousInit() {}
 void Robot::AutonomousPeriodic() {}
 
 void Robot::TeleopInit() {
+}
+
+void Robot::TeleopPeriodic() {
+}
+
+void Robot::DisabledInit() {}
+void Robot::DisabledPeriodic() {}
+
+void Robot::TestInit() {
+ }
+
+void Robot::TestPeriodic() {
+}
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
