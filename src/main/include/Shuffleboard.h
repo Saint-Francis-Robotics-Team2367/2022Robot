a1115fc7ae364ee780dd7b8f87886d33014d201a
RoastSea8 <aditya26042005@gmail.com>
2022-03-22 16:01:07 -0700

    Removed Tabs

--- src/main/include/Shuffleboard.h
+++ src/main/include/Shuffleboard.h
@@ -11,22 +11,23 @@ class Shuffleboard {
     public:
         //Drive Train
         nt::NetworkTableEntry battery = frc::Shuffleboard::GetTab("Drive Train").Add("Battery Level", (double) frc::RobotController::GetBatteryVoltage()).GetEntry();
-        nt::NetworkTableEntry distance = frc::Shuffleboard::GetTab("Drive Train").Add("Distance to Goal", 0).GetEntry();
-        nt::NetworkTableEntry angle = frc::Shuffleboard::GetTab("Drive Train").Add("Angle to Goal", 0).GetEntry();
         nt::NetworkTableEntry shooter = frc::Shuffleboard::GetTab("Drive Train").Add("Shooter UTS", false).GetEntry();
         nt::NetworkTableEntry intake = frc::Shuffleboard::GetTab("Drive Train").Add("Intake Extended", false).GetEntry();
         nt::NetworkTableEntry ball1 = frc::Shuffleboard::GetTab("Drive Train").Add("Ball 1 Loaded", false).GetEntry();
         nt::NetworkTableEntry ball2 = frc::Shuffleboard::GetTab("Drive Train").Add("Ball 2 Loaded", false).GetEntry();
 
         static bool shooter_speed() {return false;}
-        static bool ballOneLoaded() {return true;}
+        static bool ballOneLoaded() {return false;}
         static bool ballTwoLoaded() {return false;}
-        static bool intakeExtended() {return true;}
+        static bool intakeExtended() {return false;}
 
         //PIDs
         nt::NetworkTableEntry pValue = frc::Shuffleboard::GetTab("PIDs").Add("P Value", 0).GetEntry();
         nt::NetworkTableEntry iValue = frc::Shuffleboard::GetTab("PIDs").Add("I Value", 0).GetEntry();
         nt::NetworkTableEntry dValue = frc::Shuffleboard::GetTab("PIDs").Add("D Value", 0).GetEntry();
+
+        //Match Data
+        nt::NetworkTableEntry md1 = frc::Shuffleboard::GetTab("Match Data").Add("Field 1", 0).GetEntry();
 
         void refreshValues()
         {
@@ -38,8 +39,10 @@ class Shuffleboard {
             ball1.SetBoolean(ballOneLoaded);
             ball2.SetBoolean(ballTwoLoaded);
 
-            pValue.SetDouble(0);
-            iValue.SetDouble(0);
-            dValue.SetDouble(0);
+            pValue.SetDouble(1);
+            iValue.SetDouble(1);
+            dValue.SetDouble(1);
+
+            md1.SetDouble(0);
         }
 };
