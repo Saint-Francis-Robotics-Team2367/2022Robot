610bcfb1ee6fe20df6befd701ff06d70f13893ae
sidhullur12 <51142971+sidhullur12@users.noreply.github.com>
2022-03-22 18:54:58 -0700

    Removed basically everything.

--- src/main/include/Shuffleboard.h
+++ src/main/include/Shuffleboard.h
@@ -9,40 +9,13 @@
 
 class Shuffleboard {
     public:
-        //Drive Train
         nt::NetworkTableEntry battery = frc::Shuffleboard::GetTab("Drive Train").Add("Battery Level", (double) frc::RobotController::GetBatteryVoltage()).GetEntry();
-        nt::NetworkTableEntry shooter = frc::Shuffleboard::GetTab("Drive Train").Add("Shooter UTS", false).GetEntry();
         nt::NetworkTableEntry intake = frc::Shuffleboard::GetTab("Drive Train").Add("Intake Extended", false).GetEntry();
-        nt::NetworkTableEntry ball1 = frc::Shuffleboard::GetTab("Drive Train").Add("Ball 1 Loaded", false).GetEntry();
-        nt::NetworkTableEntry ball2 = frc::Shuffleboard::GetTab("Drive Train").Add("Ball 2 Loaded", false).GetEntry();
 
-        static bool shooter_speed() {return false;}
-        static bool ballOneLoaded() {return false;}
-        static bool ballTwoLoaded() {return false;}
-        static bool intakeExtended() {return false;}
-
-        //PIDs
-        nt::NetworkTableEntry pValue = frc::Shuffleboard::GetTab("PIDs").Add("P Value", 0).GetEntry();
-        nt::NetworkTableEntry iValue = frc::Shuffleboard::GetTab("PIDs").Add("I Value", 0).GetEntry();
-        nt::NetworkTableEntry dValue = frc::Shuffleboard::GetTab("PIDs").Add("D Value", 0).GetEntry();
-
-        //Match Data
-        nt::NetworkTableEntry md1 = frc::Shuffleboard::GetTab("Match Data").Add("Field 1", 0).GetEntry();
+        //static bool intakeExtended() {return false;}
 
         void refreshValues()
         {
             battery.SetDouble((double) frc::RobotController::GetBatteryVoltage());
-            //distance.SetDouble();
-            //angle.SetDouble();
-            shooter.SetBoolean(shooter_speed);
-            intake.SetBoolean(intakeExtended);
-            ball1.SetBoolean(ballOneLoaded);
-            ball2.SetBoolean(ballTwoLoaded);
-
-            pValue.SetDouble(1);
-            iValue.SetDouble(1);
-            dValue.SetDouble(1);
-
-            md1.SetDouble(0);
         }
 };
